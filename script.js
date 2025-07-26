1e764d458694674c975f4ad239b88830baeedd14
engineeredtoimperfection <207467614+engineeredtoimperfection@users.noreply.github.com>
2025-07-26 20:10:44 +0530

    Move activity data into a json config

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ function toggleLevelIndicator() {
         levelIndicatorClasses.add("faintly-visible")
     }
 }
+
+fetch('./config.json')
+    .then(res => res.json())
+    .then(config => {
+
+        const play = document.getElementById('play-activity')
+        const practice = document.getElementById('practice-activity')
+        const lastUpdated = document.getElementById('last-updated')
+
+        play.textContent = config.play_activity
+        practice.textContent = config.practice_activity
+        lastUpdated.textContent = `Last updated: ${config.last_updated}`
+
+    })
+    .catch(error => {
+        console.error('Error fetching config:', error)
+    })
