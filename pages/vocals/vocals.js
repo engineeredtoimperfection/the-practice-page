f5fbdeb74282ff493bda209b6b75ebe47a6af25c
engineeredtoimperfection <207467614+engineeredtoimperfection@users.noreply.github.com>
2025-08-27 14:42:52 +0530

    Add personal mode links on vocals and musicianship pages

--- pages/vocals/vocals.js
+++ pages/vocals/vocals.js
@@ -1,4 +1,7 @@
-fetch('../../config.json')
+loadConfig()
+
+function loadConfig() {
+    fetch('../../config.json')
     .then(res => res.json())
     .then(config => {
 
@@ -11,8 +14,37 @@ fetch('../../config.json')
         play.textContent = log.play_activity
         practice.textContent = log.practice_activity
         lastUpdated.textContent = `Last updated: ${log.last_updated}`
-        
+
+        const vocalsLinks = config.vocals.links
+
+        setLinks(vocalsLinks)
+
     })
     .catch(error => {
         console.error('Error fetching config:', error)
     })
+}
+
+function setLinks(linksOfTopics) {
+
+    const isPersonalMode = detectPersonalMode()
+
+    for (key in linksOfTopics) {
+
+        const element = document.getElementById(`${key}`)
+        
+        const linkChoices = linksOfTopics[key]
+        const link = isPersonalMode ? linkChoices.personal : linkChoices.public
+
+        element.addEventListener('click', () => {
+            window.open(link)
+        })
+
+        element.classList.add('clickable')
+    }
+
+}
+
+function detectPersonalMode() {
+    return window.location.hash === '#me'
+}
